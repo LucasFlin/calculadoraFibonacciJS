026ba144f8c8953fe62aca5017b553e4bdb12366
Lucas Flin <malucasnba@gmail.com>
2025-03-24 17:00:19 -0300

    Adicionei comentários explicando o código, e uma ultima parte que diz se o número faz ou não parte da sequencia de Fibonacci

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,11 +1,23 @@
-const {prompt } = require('vscode-websocket-alerts')
+//Chamando o comando prompt para esse arquivo
+const {prompt} = require('vscode-websocket-alerts')
+
+//Criação de variaveis, com 'numeroRecebido' sendo o número que o usuário digitar e o restante são variaveis necessárias para fazer o calculo
 let numeroRecebido = prompt("Digite um número: "), n1 = 0, n2 = 1, n3 = 1
 
+//Diz para o usuário o que o programa mostrará a seguir
 console.log(`Sequencia de Fibonacci até ${numeroRecebido} (ou o mais próximo):`)
 
+//Loop responsável por fazer o calculo de Fibonacci (o próximo número é igual a soma dos 2 anteriores) até n3 ser maior ou igual ao número informado pelo usuário
 do{
     console.log(n3)
     n1 = n2
     n2 = n3
     n3 = n1+n2
 }while(n3 <= numeroRecebido)
+
+// Compara o número digitado e o ultimo número da sequencia e mostra se o número digitado faz ou não parte da sequencia
+if(n3 == numeroRecebido){
+    console.log(`${numeroRecebido} faz parte da sequecia de Fibonacci!`)
+}else{
+    console.log(`${numeroRecebido} não faz parte da sequencia de Fibonacci.`)
+}
