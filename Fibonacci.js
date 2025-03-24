26d95303e00b5ef66c7c25c57964a3eac139b404
Lucas Flin <malucasnba@gmail.com>
2025-03-24 16:43:24 -0300

    Descobri que o Visual Studio Code não aceitava o comando prompt do JS, então adicionei os arquivos necessários para o comando funcionar usando 'npm i vscode-websocket-alerts' e uma extenção do VSCode

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,1 +1,11 @@
-console.log("Hello World")
+const {prompt } = require('vscode-websocket-alerts')
+let numeroRecebido = prompt("Digite um número: "), n1 = 0, n2 = 1, n3 = 1
+
+console.log(`Sequencia de Fibonacci até ${numeroRecebido} (ou o mais próximo):`)
+
+do{
+    console.log(n3)
+    n1 = n2
+    n2 = n3
+    n3 = n1+n2
+}while(n3 <= numeroRecebido)
